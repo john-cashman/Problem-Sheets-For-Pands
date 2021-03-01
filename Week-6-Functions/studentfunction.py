ccf51a8a4ad1b5fa882e3ec40737e420b9abb5a7
john-cashman <65048603+john-cashman@users.noreply.github.com>
2021-03-01 19:25:39 +0000

    functions lab work

--- Week-6-Functions/studentfunction.py
+++ Week-6-Functions/studentfunction.py
@@ -2,29 +2,51 @@
 #The function then returns what the user chose.
 #Author: John Cashman
 
-def displayMenu ():
-    print("what would you like to do?")
-    print("\t (a) Add a new student")
-    print("\t (v) View Students")
-    print("\t (q) Quit")
-    choice = input("Type one letter a/v/q:").strip() #strip gets rid of any spaces returned by the user
-
+def displayMenu():
+    print("What would you like to do?")
+    print("\t(a) Add new student")
+    print("\t(v) View students")
+    print("\t(q) Quit")
+    choice = input("Type one letter (a/v/q):").strip()
     return choice
-def doAdd():
-    print("in adding")
-def doView():
-    print("in viewing")
 
-#this is the main program
 
+def doAdd(students):
+    currentStudent = {}
+    currentStudent["name"]=input("Enter name :")
+    currentStudent["modules"]= readModules()
+    students.append(currentStudent)
+
+def readModules():
+    modules = []
+    moduleName = input("\tEnter the first Module name (blank to quit) :").strip()
+    
+    while moduleName != "":
+        module = {}
+        module["name"]= moduleName
+        module["grade"]=int(input("\t\tEnter grade:"))
+        modules.append(module)
+        moduleName = input("\tEnter next module name (blank to quit) :").strip()
+
+    return modules
+def displayModules(modules):
+    print ("\tName   \tGrade")
+    for module in modules:
+        print("\t{}  \t{}".format(module["name"], module["grade"])) 
+
+
+def doView(students):
+    for currentStudent in students:
+        print(currentStudent["name"])
+        displayModules(currentStudent["modules"]);
+
+students = []
 choice = displayMenu()
 while(choice != 'q'):
     if choice == 'a':
-        doAdd()
-    elif choice == 'y':
-        doView()
+        doAdd(students)
+    elif choice == 'v':
+        doView(students)
     elif choice !='q':
-        print("\n\n please select either a,v or q")
-choice=commandList()
-
-#print("you chose {} ".format(choice))
+        print("\n\nplease select either a,v or q")
+    choice=displayMenu()
